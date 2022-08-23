ba6127b944eee42adbb9206d416b9e7e2c0f8ee6
Aksaev <mail-ax@ya.ru>
2022-08-23 16:24:35 +0400

    Выполнил домашнее задание "Переменные" Урок 1.

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,56 @@
 public class Main {
     public static void main(String[] args) {
         // Задача 1
-        byte byteNum = 1;
-        short shortNum = 2;
-        int intNum = 3;
-        long longNum = 4L;
-        float floatNum = 5.6f;
-        double doubleNum = 7.8;
-        char charNum = 99;
-        boolean boolNum = 10 > 2;
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
 
         // Задача 2
-        double boxersWeight1 = 78.2;
-        double boxersWeight2 = 82.7;
-        double totalWeight = boxersWeight1 + boxersWeight2;
-        double differenceWeight = boxersWeight2 - boxersWeight1;
+        dog = dog + 4;
+        cat = cat + 4;
+        paper =paper + 4;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        // Задача 3
+        dog = dog - (int)3.5;
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        // Задача 4
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+        //Задача 5
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        // Задача 6
+        var boxersWeight1 = 78.2;
+        var boxersWeight2 = 82.7;
+        var totalWeight = boxersWeight1 + boxersWeight2; // Общий вес бойцов
+        var differenceWeight = boxersWeight2 - boxersWeight1; // Разница между весами бойцов
         System.out.println("Общий вес двух бойцов " + totalWeight + " кг");
+        System.out.println("Разница между весами бойцов " + differenceWeight + " кг");
+
+        // Задача 7
         System.out.println("Разница между весами бойцов " + differenceWeight + " кг");
 
         // Задача 3
